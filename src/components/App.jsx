c95f4af58abef7c962a1b59d4fa538e221bbe7dd
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-05-14 15:33:25 +0200

    First commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+import { useState, useEffect } from 'react';
+import css from './App.module.css';
+import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import ContactList from './Contacts/Contacts';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = contact => {
+    setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+
+  const filterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const filterContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const filteredContacts = filterContacts();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <Form contacts={contacts} onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterChange} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
 };
